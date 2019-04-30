e7022dd38c109816be6a2ebdb40808bdfb7fc821
master <49400874@qq.com>
2019-04-30 16:29:45 +0800

    common service

---
+++ src/main/java/me/jessyan/armscomponent/commonservice/dao/DaoHelper.java
@@ -0,0 +1,32 @@
+package me.jessyan.armscomponent.commonservice.dao;
+
+import java.util.List;
+
+import me.jessyan.armscomponent.commonservice.CommonApp;
+
+/**
+ * Created by zhou on 2018/11/10.
+ */
+
+public class DaoHelper {
+
+    public static void saveCls(List<ClsBean> clsBeans){
+        ClsBeanDao dao = CommonApp.getInstance().getDaoSession().getClsBeanDao();
+        dao.insertOrReplaceInTx(clsBeans);
+    }
+
+    public static void saveInfo(List<InfoBean> infoBeans){
+        InfoBeanDao dao = CommonApp.getInstance().getDaoSession().getInfoBeanDao();
+        dao.insertOrReplaceInTx(infoBeans);
+    }
+
+    public static InfoBean fetchInfoBean(String tag){
+        InfoBeanDao dao = CommonApp.getInstance().getDaoSession().getInfoBeanDao();
+        return dao.load(tag);
+    }
+
+    public static List<InfoBean> fetchInfoBeans(){
+        InfoBeanDao dao = CommonApp.getInstance().getDaoSession().getInfoBeanDao();
+        return dao.queryBuilder().where(InfoBeanDao.Properties.Tag.like("1%")).orderAsc(InfoBeanDao.Properties.Tag).limit(8).list();
+    }
+}
